235f587ccedba620eacf6c8e6562d9fe8f64fe35
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-07-29 17:46:09 +0530

    Add search functionality and pagination controls to All Users page

--- src/app/app/admin/users/page.tsx
+++ src/app/app/admin/users/page.tsx
@@ -1,4 +1,5 @@
 import Breadcrumb from '@/components/Breadcrumbs/Breadcrumb'
+import InputGroup from '@/components/FormElements/InputGroup';
 import AllUsersTable from '@/components/Tables/users-table'
 import { Button } from '@/components/ui-elements/button'
 import { Metadata } from 'next';
@@ -18,7 +19,43 @@ const AllUsersPage = () => {
         className='my-5'
         size={"small"}
       />
+
+      <div
+        className='mb-5'
+      >
+        <InputGroup
+          label='Search User'
+          placeholder='john@email.com'
+          type='email'
+          className='rounded-[10px] bg-white px-7.5 py-4 shadow-1 dark:bg-gray-dark dark:shadow-card'
+        />
+      </div>
+
       <AllUsersTable />
+
+      <div
+        className='w-full mt-5'
+      >
+        <div
+          className='flex flex-nowrap gap-3 items-center justify-center w-full'
+        >
+          <Button
+            label='Prev'
+            variant={"primary"}
+            shape={"rounded"}
+            className='bg-white text-black shadow-1 dark:bg-gray-dark dark:shadow-card'
+          />
+          <p
+            className='text-black dark:text-white font-semibold'
+          >1</p>
+          <Button
+            label='Next'
+            variant={"primary"}
+            shape={"rounded"}
+            className='bg-white text-black shadow-1 dark:bg-gray-dark dark:shadow-card'
+          />
+        </div>
+      </div>
     </div>
   )
 }
